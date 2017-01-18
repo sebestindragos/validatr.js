73d569ef0c2ca9e71d622971d243379c66a600e2
Dragos Sebestin <sebestin.dragos@gmail.com>
2017-01-18 23:21:47 +0200

    [master] Added validationResult class and exported main object from module.

---
+++ src/lib/validationResult.ts
@@ -0,0 +1,12 @@
+/**
+ * Class used for merging validation errors.
+ * 
+ * @author Dragos Sebestin
+ */
+export class ValidationResult {
+
+  /**
+   * Class constructor.
+   */
+  constructor () {}
+}
