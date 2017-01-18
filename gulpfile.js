14a0a2fe21c758ecc3398911c38801f6295ef8a2
Dragos Sebestin <sebestin.dragos@gmail.com>
2017-01-18 23:17:26 +0200

    [master] Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const gulp = require('gulp');
+const typescript = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const rm = require('gulp-rimraf');
+const tslint = require('gulp-tslint');
+const runSequence = require('run-sequence');
+const mocha = require('gulp-mocha');
+
+/**
+ * Define tasks.
+ */
+gulp.task('clean', () => {
+  return gulp.src(['build/'], {read: false}).pipe(rm());
+});
+
+gulp.task('tslint', () => {
+  return gulp.src([
+    'src/**/*.ts'
+  ])
+  .pipe(tslint({
+    formatter: 'verbose',
+    configuration: 'tslint.json'
+  }))
+  .pipe(tslint.report({
+    summarizeFailureOutput: true
+  }));
+});
+
+gulp.task('compile', () => {
+  let ts = typescript.createProject('tsconfig.json');
+  return gulp.src([
+    'src/**/*.ts'
+  ])
+  .pipe(sourcemaps.init())
+  .pipe(ts())
+  .pipe(sourcemaps.write('.', {
+    sourceRoot: (file) => {
+      return file.cwd + '/src'
+    }
+  }))
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('tests', () => {
+  return gulp.src([
+    'build/tests/**/*.js'
+  ], {read: false})
+    .pipe(mocha());
+});
+
+/**
+ * Register tasks.
+ */
+gulp.task('build', (callback) => {
+  runSequence(
+    'clean',
+    'tslint',
+    'compile',
+    'tests',
+    callback
+  );
+});
+
+gulp.task('default', ['build']);
